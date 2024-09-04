48f26c91d9041fddcd31aec66c88bf9d5a53b498
Test <Test@test.test>
2024-09-04 13:30:56 +0700

    add core of frontend part

---
+++ frontend/src/components/App/App.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+function App() {return (<div>Hello from base app</div>)}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css)$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
+}
