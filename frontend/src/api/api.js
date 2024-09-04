63e494eb178e84623d3d8cf68d88c62e859579c0
Test <Test@test.test>
2024-09-04 17:32:39 +0700

    add get objects form api methods

---
+++ frontend/src/api/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+
+const baseUrl = "http://localhost:8000";
+
+
+const getTasks = async () => {
+    const response = await axios.get(`${baseUrl}`);
+    return response.data;
+};
+
+const getTask = async (id) => {
+    const response = await axios.get(`${baseUrl}/${id}`);
+    return response.data;
+};
+
+
+const createTask = async (task) => {
+    const response = await axios.post(`${baseUrl}`, task);
+    return response.data;
+};
+
+
+const updateTask = async (id, task) => {
+    const response = await axios.put(`${baseUrl}/${id}`, task);
+    return response.data;
+};
+
+
+const deleteTask = async (id) => {
+    const response = await axios.delete(`${baseUrl}/${id}`);
+    return response.data;
+};
+
+
+export { getTasks, getTask, createTask, updateTask, deleteTask }
