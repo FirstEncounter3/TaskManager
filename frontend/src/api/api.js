a46e3aabb98ba4d9df87e6996598abf1d2c9be6b
FirstEncounter3 <Test@test.test>
2024-09-05 17:38:26 +0700

    fix wrong routes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -10,25 +10,25 @@ const getTasks = async () => {
 };
 
 const getTask = async (id) => {
-    const response = await axios.get(`${baseUrl}/${id}`);
+    const response = await axios.get(`${baseUrl}/${id}/`);
     return response.data;
 };
 
 
 const createTask = async (task) => {
-    const response = await axios.post(`${baseUrl}/create`, task);
+    const response = await axios.post(`${baseUrl}/create/`, task);
     return response.data;
 };
 
 
 const updateTask = async (id, task) => {
-    const response = await axios.put(`${baseUrl}/${id}`, task);
+    const response = await axios.put(`${baseUrl}/${id}/`, task);
     return response.data;
 };
 
 
 const deleteTask = async (id) => {
-    const response = await axios.delete(`${baseUrl}/${id}`);
+    const response = await axios.delete(`${baseUrl}/${id}/`);
     return response.data;
 };
 
