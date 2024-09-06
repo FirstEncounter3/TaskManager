5c82ba8f56181c004833e14afd02f828673c4698
FirstEncounter3 <needenergy27@gmail.com>
2024-09-06 17:39:56 +0700

    add all info on the task

--- frontend/src/components/TaskInfo/TaskInfo.jsx
+++ frontend/src/components/TaskInfo/TaskInfo.jsx
@@ -3,7 +3,17 @@ import React, { useState, useEffect } from "react";
 
 const TaskInfo = ({taskInfo}) => {
     return (
-        <div>{taskInfo.description}</div>
+        <div className="task-info-wrapper">
+            <h1>{taskInfo.title}</h1>    
+            <p>Описание: {taskInfo.description}</p>
+            <p>Ответственный: {taskInfo.responsible}</p>
+            <p>Время создания: {taskInfo.created_at}</p>
+            <p>Статус: {taskInfo.status}</p>
+            <p>Планируемое время: {taskInfo.planned_effort}</p>
+            <p>Фактическое время: {taskInfo.actual_effort}</p>
+            <p>Завершена: {taskInfo.completed_at ? taskInfo.completed_at : "Нет"}</p>
+            <p>Родительская задача: {taskInfo.parent ? taskInfo.parent : "Нет"}</p>
+        </div>
     )
 }
 
