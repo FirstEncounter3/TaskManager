9bb610005cc20fb55af2c915c9d13362828df97c
First_Encounter <needenergy27@gmail.com>
2024-09-10 00:00:53 +0700

    add asynchronous task execution, refactoring

--- frontend/src/components/ModalDelete/ModalDelete.jsx
+++ frontend/src/components/ModalDelete/ModalDelete.jsx
@@ -5,23 +5,34 @@ import { deleteTask } from "../../api/api";
 import "./ModalDelete.css";
 
 const ModalDelete = ({ show, onClose, taskInfo }) => {
-    if (!show) return null;
+  if (!show) return null;
 
-    const acceptDelete = () => {
-        deleteTask(taskInfo.id);
+  const acceptDelete = async () => {
+    try {
+        await deleteTask(taskInfo.id);
         onClose();
+        window.location.reload();
+    } catch (error) {
+      console.error(error);
+      alert(`Ошибка при удалении ${error.message}`);
     }
+  };
 
-    return (
-        <div className="modal-delete-wrapper">
-            <p>{`Вы уверены, что хотите удалить задачу ${taskInfo.title}`}</p>
-            <div className="modal-delete-buttons">
-                <button onClick={acceptDelete}>Да</button>
-                <button onClick={onClose}>Отмена</button>
-            </div>
+  return (
+    <div className="modal-delete-wrapper">
+      <div className="modal-delete">
+        <p>{`Вы уверены, что хотите удалить задачу ${taskInfo.title}`}</p>
+        <div className="task-info-buttons">
+          <button className="task-button delete" onClick={acceptDelete}>
+            Да
+          </button>
+          <button className="task-button" onClick={onClose}>
+            Отмена
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-
-export default ModalDelete
+export default ModalDelete;
