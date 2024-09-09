ac4aa7a883b887dc0180bb60650649842673f114
First_Encounter <needenergy27@gmail.com>
2024-09-10 00:03:36 +0700

    add refactoring, enabling the ability to edit a task

--- frontend/src/components/TaskInfo/TaskInfo.jsx
+++ frontend/src/components/TaskInfo/TaskInfo.jsx
@@ -1,41 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import { getTask } from "../../api/api";
 
 import ModalDelete from "../ModalDelete/ModalDelete";
+import ModalCreate from "../ModalCreate/ModalCreate";
 
 import "./TaskInfo.css";
 
 const TaskInfo = ({ taskInfo }) => {
-    const [modalShow, setModalShow] = useState(false);
+  const [modalShowCreate, setModalShowCreate] = useState(false);
+  const [modalShowDelete, setModalShowDelete] = useState(false);
+  const [parentTitle, setParentTitle] = useState("");
 
-    const modalOpen = () => {
-        setModalShow(true)
-    };
+  useEffect(() => {
+    if (taskInfo.parent) {
+      getTask(taskInfo.parent)
+        .then((data) => {
+          setParentTitle(data.title);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [taskInfo]);
 
-    const modalClose = () => {
-        setModalShow(false)
-    };
+  const modalCreateOpen = () => {
+    setModalShowCreate(true);
+  };
 
-    return (
-        <>
-            <div className="task-info-wrapper">
-                <h1>{taskInfo.title}</h1>
-                <p>Описание: {taskInfo.description}</p>
-                <p>Ответственный: {taskInfo.responsible}</p>
-                <p>Время создания: {taskInfo.created_at}</p>
-                <p>Статус: {taskInfo.status}</p>
-                <p>Планируемое время: {taskInfo.planned_effort}</p>
-                <p>Фактическое время: {taskInfo.actual_effort}</p>
-                <p>Завершена: {taskInfo.completed_at ? taskInfo.completed_at : "Нет"}</p>
-                <p>Родительская задача: {taskInfo.parent ? taskInfo.parent : "Нет"}</p>
-                <div className="task-info-buttons">
-                    <button className="task-button">Редактировать</button>
-                    <button className="task-button delete" onClick={modalOpen}>Удалить</button>
-                </div>
-            </div>
-            <ModalDelete show={modalShow} onClose={modalClose} taskInfo={taskInfo} />
-        </>
-    )
-}
+  const modalCreateClose = () => {
+    setModalShowCreate(false);
+  };
 
+  const modalDeleteOpen = () => {
+    setModalShowDelete(true);
+  };
+
+  const modalDeleteClose = () => {
+    setModalShowDelete(false);
+  };
+
+  return (
+    <>
+      <div className="task-info-wrapper">
+        <h1>{taskInfo.title}</h1>
+        <hr></hr>
+        <div className="task-info-content">
+          <p>
+            <span>Описание: </span>
+            {taskInfo.description}
+          </p>
+          <p>
+            <span>Ответственный: </span>
+            {taskInfo.responsible}
+          </p>
+          <p>
+            <span>Время создания: </span>
+            {taskInfo.created_at}
+          </p>
+          <p>
+            <span>Статус: </span>
+            {taskInfo.status}
+          </p>
+          <p>
+            <span>Планируемое время: </span>
+            {taskInfo.planned_effort}
+          </p>
+          <p>
+            <span>Фактическое время: </span>
+            {taskInfo.actual_effort}
+          </p>
+          <p>
+            <span>Завершена: </span>
+            {taskInfo.completed_at ? taskInfo.completed_at : "Нет"}
+          </p>
+          <p>
+            <span>Родительская задача: </span>
+            {taskInfo.parent ? parentTitle : "Нет"}
+          </p>
+        </div>
+        <div className="task-info-buttons">
+          <button
+            className="task-button"
+            onClick={modalCreateOpen}
+            disabled={taskInfo.status === "completed"}
+          >
+            Редактировать
+          </button>
+          <button className="task-button delete" onClick={modalDeleteOpen}>
+            Удалить
+          </button>
+        </div>
+      </div>
+      <ModalDelete
+        show={modalShowDelete}
+        onClose={modalDeleteClose}
+        taskInfo={taskInfo}
+      />
+      <ModalCreate
+        show={modalShowCreate}
+        onClose={modalCreateClose}
+        taskInfo={taskInfo}
+        editTask={true}
+      />
+    </>
+  );
+};
 
 export default TaskInfo;
